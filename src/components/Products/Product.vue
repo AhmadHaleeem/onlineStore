fa6ca5fc8355bbb6a5d4fe0aa700818de5ed87f9
AhmadHaleeem <coldman2099@gmail.com>
2018-10-29 15:39:42 +0100

    Modify the products pages and install vuex

--- src/components/Products/Product.vue
+++ src/components/Products/Product.vue
@@ -1,23 +1,79 @@
 <template>
   <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>Product</h1>
-      </v-flex>
-    </v-layout>
+    <section class="product mt-3 elevation-3">
+      <v-layout row wrap>
+        <v-flex xs12 lg6>
+          <img src="https://rukminim1.flixcart.com/flap/1400/1400/image/7c0b69.jpg?q=50" class="product_img">
+        </v-flex>
+
+        <v-flex xs12 lg6>
+          <div class="product_info">
+            <h5 class="product_title display-1 mb-3 mt-3">Lorem Lorem</h5>
+            <p class="product_category title">
+              <span class="product_title">Vendor: </span>Lorem
+            </p>
+            <p class="product_price title">
+              <span class="product_title">price: </span>$ 1000
+            </p>
+            <p class="product_color title">
+              <span class="product_title">Color: </span>
+              <span :title="'purple'" :style="{ backgroundColor: 'purple' }" class="product_color__bg"></span>
+            </p>
+            <p class="title">
+              <span class="product_title">Material: </span>Lorem
+            </p>
+            <div class="title mb-5">
+              <p class="product_title mb-2">Description: </p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled
+            </div>
+            <v-btn color="primary" class="headline">Edit</v-btn>
+            <v-btn color="primary" class="headline">Buy</v-btn>
+          </div>
+        </v-flex>
+      </v-layout>
+    </section>
   </v-container>
 </template>
 
 <script>
   export default {
-    data () {
-      return {
-
-      }
+    data() {
+      return {}
     }
   }
 </script>
 
 <style scoped>
+  .product {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    border: 1px solid #1c77ad;
+    padding: 30px;
+    margin-bottom: 10px;
+  }
 
+  .product_img {
+    height: 100%;
+    width: 550px;
+  }
+
+  .product_info {
+    margin-left: 50px;
+  }
+  .product_title {
+    font-size: 1.6rem;
+    font-weight: bold;
+    color: #1875d0;
+    margin-bottom: 0;
+  }
+  .product_price {
+    color: #f00;
+  }
+  .product_color__bg {
+    display: inline-block;
+    width: 50px;
+    height: 20px;
+    border: 1px solid #2b2b2b;
+    border-radius: 10px;
+  }
 </style>
--- src/components/Products/ProductList.vue
+++ src/components/Products/ProductList.vue
@@ -1,8 +1,27 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>My Products</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">My Products</h1>
+        <v-card class="elevation-10 mb-2" v-for="(product, index) in products" :key="index">
+          <v-layout>
+            <v-flex xs4>
+              <v-card-media :src="product.imageSrc" height="250px"></v-card-media>
+            </v-flex>
+
+            <v-flex xs8>
+              <v-card-text>
+                <h2 class="text--primary">{{ product.title }}</h2>
+                <p>{{ product.description }}</p>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn class="info" :to="`/product/${product.id}`">Open</v-btn>
+              </v-card-actions>
+            </v-flex>
+
+          </v-layout>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -10,9 +29,32 @@
 
 <script>
   export default {
-    data () {
+    data() {
       return {
-
+        products: [
+          {
+            id: '1',
+            title: 'Lenovo Legion Y520',
+            vendor: 'Lenovo',
+            color: 'Black',
+            material: 'metal/plastic',
+            description: 'Intel core i5 7400HQ MHz/15.6',
+            price: 784,
+            promo: false,
+            imageSrc: 'https://rukminim1.flixcart.com/flap/1400/1400/image/7c0b69.jpg?q=50'
+          },
+          {
+            id: '2',
+            title: 'Asus FX503VD',
+            vendor: 'Asus',
+            color: 'white',
+            material: 'plastic',
+            description: 'Intel core i5 7400HQ MHz/15.6',
+            price: 460,
+            promo: false,
+            imageSrc: 'https://c.s-microsoft.com/en-us/CMSImages/Windows10_ViewAll__hero_1920.jpg?version=9827798b-32be-675f-4a86-ae7dca0d2e19'
+          },
+        ]
       }
     }
   }
